0fe218e7fdb988e66eb603d49c1744bc5a2f54f3
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2025-04-04 13:26:39 +0300

    Убрал лишний код из main

--- MyVector/main.cpp
+++ MyVector/main.cpp
@@ -5,60 +5,6 @@
 #include <iomanip>
 #include <sstream>
 
-struct Spy
-{
-	Spy()
-	{
-		std::cout << "Spy Constructed\n";
-	}
-
-	Spy(Spy&& other) noexcept
-		: data(other.data)
-	{
-		std::cout << "Spy Moved\n";
-	}
-
-	Spy(const Spy& other)
-		: data(other.data)
-	{
-		std::cout << "Spy Copied\n";
-	}
-
-	~Spy()
-	{
-		std::cout << "Spy Destroyed\n";
-		delete[] dataArr;
-	}
-
-	Spy& operator=(const Spy& other)
-	{
-		return *this;
-	}
-
-	double data = 666;
-	int* dataArr = new int[10];
-};
-
-void PrintSpy(const Vector<Spy>& vec)
-{
-	std::cout << "-----Reversed iterator for-----" << std::endl;
-	for (Vector<Spy>::TConstReverseIterator it = vec.rbegin(); it != vec.rend(); ++it)
-	{
-		std::cout << "Spy " << it->data << std::endl;
-	}
-	std::cout << "-----Range-based for-----------" << std::endl;
-	for (auto& spy : vec)
-	{
-		std::cout << "Spy " << spy.data << std::endl;
-	}
-	std::cout << "-----Index for-----------------" << std::endl;
-	for (size_t i = 0; i < vec.Size(); ++i)
-	{
-		std::cout << "Spy " << vec[i].data << std::endl;
-	}
-	std::cout << "-------------------------------" << std::endl;
-}
-
 int main()
 {
 	Vector<double> numbers;
